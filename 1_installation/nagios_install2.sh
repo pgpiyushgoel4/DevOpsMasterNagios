98a1fd32b93979076f73c2e047587d4c155f1b3a
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2020-05-15 13:20:40 +0530

    Update nagios_install2.sh

--- 1_installation/nagios_install2.sh
+++ 1_installation/nagios_install2.sh
@@ -2,7 +2,7 @@ sudo htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
 sudo a2enconf nagios
 sudo a2enmod cgi rewrite
 sudo service apache2 restart
-wget http://www.nagios-plugins.org/download/nagios-plugins-2.2.1.tar.gz
+sudo wget http://www.nagios-plugins.org/download/nagios-plugins-2.2.1.tar.gz
 sudo tar xzf nagios-plugins-2.2.1.tar.gz
 cd nagios-plugins-2.2.1
 sudo ./configure --with-nagios-user=nagios --with-nagios-group=nagios --with-openssl
